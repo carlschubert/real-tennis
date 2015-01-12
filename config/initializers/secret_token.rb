82b22babe8623da24af6c25632ec1f93547a8767
carlschubert <carl.schubert@gmail.com>
2015-01-11 23:16:04 -0500

    add rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealTennis::Application.config.secret_token = '3b1a0861948f1b04728c20a43c1ef27a4a8f2ddb447d6605e55a8c0c711e963aee27eaf1eadcd9637f9859429eb3203ffc65bc0b91858bca02406cb96b641f54'
