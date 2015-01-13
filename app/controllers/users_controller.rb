0045de2623ad9ffe9c26e1e4adf07d46751a2fef
Carl Schubert <carl.schubert@gmail.com>
2015-01-12 21:13:01 -0500

    try to fix signup by defining user, did not work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,19 @@ class UsersController < ApplicationController
 
 
   def create
+    @user = User.new
     100.times do
       p '-----------------------------------'
     end
     p params
     p params[:user]
-    binding.pry
+
     User.create(auth_params)
     redirect_to root_path
   end
 
   def auth_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit( :password, :password_confirmation)
   end
 
   def signup
